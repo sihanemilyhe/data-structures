d1a9a06835098f2ed1db8dd607496a23b4f0d1e9
sihanemilyhe <she@stu.naperville203.org>
2026-09-21 15:09:57 -0500

    floodfill stuff

--- Chapter 15 Activities/FloodFillQueue/src/Grid.java
+++ Chapter 15 Activities/FloodFillQueue/src/Grid.java
@@ -1,16 +1,19 @@
-. . .
+import java.util.Stack;
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    Stack<Pair> pairs = new Stack<>();
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        Pair block = new Pair(row, column);
+        while (block.getRow()!=0&&block.getColumn()!=0){
+            
+        }
     }
 
     public String toString()
